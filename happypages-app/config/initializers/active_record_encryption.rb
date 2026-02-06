5696f5810e8d4115c595ca3f86cb51597c6bc704
Alex McConnell <lexmcconnell@gmail.com>
2026-02-06 11:16:30 +0000

    Enable support_unencrypted_data for PII encryption transition
    Existing records have plaintext email/first_name values. Rails needs
    support_unencrypted_data = true to read them before re-encryption.

--- happypages-app/config/initializers/active_record_encryption.rb
+++ happypages-app/config/initializers/active_record_encryption.rb
@@ -6,3 +6,7 @@ if ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"].present?
   Rails.application.config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
   Rails.application.config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
 end
+
+# Allow reading plaintext data from columns that now have `encrypts`
+# Required during transition while existing records are re-encrypted
+Rails.application.config.active_record.encryption.support_unencrypted_data = true
