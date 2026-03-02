72337ee743d7dd6fdd9c2cf6809b24e4db436c2e
Alex McConnell <lexmcconnell@gmail.com>
2026-03-02 11:24:04 +0000

    Make allowed hosts and mailer host configurable via env vars

--- happypages-app/config/environments/production.rb
+++ happypages-app/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "app.happypages.co" }
+  config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST", "app.happypages.co") }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
@@ -80,9 +80,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "app.happypages.co"
-  ]
+  config.hosts = ENV.fetch("ALLOWED_HOSTS", "app.happypages.co").split(",").map(&:strip)
 
   # Skip DNS rebinding protection for the default health check endpoint.
   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
