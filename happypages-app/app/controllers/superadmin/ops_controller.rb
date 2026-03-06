1252369aef097a6132738a6f227fd17eda7f65df
Alex McConnell <lexmcconnell@gmail.com>
2026-03-06 17:30:26 +0000

    Switch ops dashboard to client-side fetch from local Node server
    Browser fetches directly from 127.0.0.1:3333 instead of Rails
    proxying server-side. Works from any environment (staging/prod)
    as long as the local dashboard server is running.

--- happypages-app/app/controllers/superadmin/ops_controller.rb
+++ happypages-app/app/controllers/superadmin/ops_controller.rb
@@ -1,12 +1,4 @@
 class Superadmin::OpsController < Superadmin::BaseController
   def index
-    dashboard_url = ENV.fetch("OPS_DASHBOARD_URL", "http://127.0.0.1:3333")
-    uri = URI("#{dashboard_url}/?embed=1&dashboardUrl=#{CGI.escape(dashboard_url)}")
-    response = Net::HTTP.start(uri.hostname, uri.port, read_timeout: 5, open_timeout: 3) { |http| http.request(Net::HTTP::Get.new(uri)) }
-    @dashboard_html = response.body
-    @dashboard_url = dashboard_url
-  rescue => e
-    Rails.logger.error("[OPS] #{e.class}: #{e.message}")
-    @dashboard_html = nil
   end
 end
